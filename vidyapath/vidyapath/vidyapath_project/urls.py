c8c713ede8494f2e0e0d10e15638eff6cc7ed296
Shruti785 <160528343+Shruti785@users.noreply.github.com>
2025-04-17 00:53:49 +0530

    Delete vidyapath/vidyapath directory

--- vidyapath/vidyapath/vidyapath_project/urls.py
+++
@@ -1,5 +0,0 @@
-'''from .views import user_login, user_logout, dashboard
-
-path('login/', user_login, name='login'),
-path('logout/', user_logout, name='logout'),
-path('dashboard/', dashboard, name='dashboard'),'''
